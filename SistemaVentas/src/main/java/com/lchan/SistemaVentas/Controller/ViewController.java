11fc6383c0bcb65a54a03e3fa69be63c7cba63c4
lchan-2024540 <lchan-2024540@kinal.edu.gt>
2026-04-13 22:59:01 -0600

    Se agregó el thymeleaf para la página

--- SistemaVentas/src/main/java/com/lchan/SistemaVentas/Controller/ViewController.java
+++ SistemaVentas/src/main/java/com/lchan/SistemaVentas/Controller/ViewController.java
@@ -1,4 +1,43 @@
 package com.lchan.SistemaVentas.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ViewController {
+
+    @GetMapping("/")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
+
+    @GetMapping("/clientes")
+    public String clientes() {
+        return "clientes";
+    }
+
+    @GetMapping("/productos")
+    public String productos() {
+        return "productos";
+    }
+
+    @GetMapping("/ventas")
+    public String ventas() {
+        return "ventas";
+    }
+
+    @GetMapping("/detalle-venta")
+    public String detalleVenta() {
+        return "detalle-venta";
+    }
+
+    @GetMapping("/usuarios")
+    public String usuarios() {
+        return "usuarios";
+    }
 }

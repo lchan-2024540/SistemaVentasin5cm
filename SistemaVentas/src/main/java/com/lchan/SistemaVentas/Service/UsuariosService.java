4f851eb0298157411e67f5c399faaf0d852ea766
lchan-2024540 <lchan-2024540@kinal.edu.gt>
2026-04-14 23:47:13 -0600

    fix: se hicieron cambios para integrar los métodos correctamente

--- SistemaVentas/src/main/java/com/lchan/SistemaVentas/Service/UsuariosService.java
+++ SistemaVentas/src/main/java/com/lchan/SistemaVentas/Service/UsuariosService.java
@@ -1,13 +1,16 @@
 package com.lchan.SistemaVentas.Service;
+
 import com.lchan.SistemaVentas.Entity.Usuarios;
-import org.springframework.stereotype.Service;
 import java.util.List;
 
-@Service
 public interface UsuariosService {
     List<Usuarios> getAllUsuarios();
     Usuarios getUsuariosById(Integer codigoUsuario);
     Usuarios saveUsuarios(Usuarios usuarios) throws RuntimeException;
     Usuarios updateUsuarios(Integer codigoUsuario, Usuarios usuarios);
     void deleteUsuarios(Integer codigoUsuario);
+    Usuarios login(String username, String password);
+    Usuarios registrar(Usuarios usuarios) throws RuntimeException;
+    Usuarios updateFoto(Integer codigoUsuario, byte[] foto);
+    byte[] getFoto(Integer codigoUsuario);
 }
